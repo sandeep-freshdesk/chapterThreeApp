204379bf57dae174805528152c9f5fa50c94d976
sandeep-freshdesk <sandeep@freshdesk.com>
2017-01-18 15:28:27 +0530

    adding pages controller file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
+  puts "in PagesController"
   def home
+  	puts "came in home method"
   end
 
   def contact
+  	puts "came in contact method"
   end
 
 end
